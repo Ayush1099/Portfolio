b5f510c1e3730a3cccf99794f8225a5dc6b3efd4
Ayush1099 <ayush99verma@gmail.com>
2023-10-01 16:33:04 +0530

    Pushing Files

---
+++ src/data/constants.js
@@ -0,0 +1,179 @@
+export const Bio = {
+  name: "Ayush Verma",
+  roles: "Software Developer",
+  description:
+    "I have 2+ years of experience. Experienced in developing scalable and secure Microservices using C# in ASP.NET Core Web API. Skilled in JavaScript and ReactJS for building responsive Front-end. Excellent analytical, problem-solving skills and commited to delivering high-quality code with a track record of meeting and exceeding project goals. Passion for learning new technologies to improve technical skill set. Always eager to take on new challenges with a positive attitude and a growth mindset.",
+  github: "https://github.com/Ayush1099",
+  resume: "https://drive.google.com/file/d/1Cfvr6fqGU1poEeJmeHXRHBAbcNRngY2A/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ayush-verma-110283190/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:"https://cdn.worldvectorlogo.com/logos/react-2.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "C#",
+        image: "https://cdn.worldvectorlogo.com/logos/c--4.svg"
+      },
+      {
+        name: "C++",
+        image: "https://cdn.worldvectorlogo.com/logos/c.svg"
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+      {
+        name: "MySQL",
+        image:"https://cdn.worldvectorlogo.com/logos/mysql-logo.svg",
+      },
+      {
+        name: "REST API",
+        image: "https://cdn-icons-png.flaticon.com/512/10195/10195226.png?ga=GA1.1.238019527.1696140306"
+      },
+      {
+        name: "Asp.Net Core",
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "GitHub",
+        image:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Postman",
+        image:"https://cdn.worldvectorlogo.com/logos/postman.svg",
+      },
+      {
+        name: "VS Code",
+        image:"https://cdn.worldvectorlogo.com/logos/visual-studio-code-1.svg",
+      },
+      {
+        name: "Visual Studio",
+        image:"https://cdn.worldvectorlogo.com/logos/visual-studio-2013.svg",
+      },
+      {
+        name: "Azure DevOps",
+        image:"https://149765861.v2.pressablecdn.com/wp-content/uploads/2020/01/devops.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://static.vecteezy.com/system/resources/previews/020/336/451/original/infosys-logo-infosys-icon-free-free-vector.jpg",
+    role: "System Engineer",
+    company: "Infosys Ltd.",
+    date: "May 2022 - Present",
+    desc: "Collaborated with cross-functional teams in a fast-paced Agile environment, providing timely user story updates in daily scrum meetings. Developed scalable and secure Microservices using C# in ASP.NET Core Web API 6.0, following CQRS design pattern. Unit tested Microservices using XUnit, achieving code coverage above 80%. Created responsive user interfaces using ReactJS for SPA development. Integrated Swagger for API documentation, testing, analyzing and debugging/troubleshooting functional and technical issues. Implemented JWT mechanism for authentication and authorization. Consulted with QA engineers to test specific work items and resolve bugs. Mentored team members on assigned tasks and provided constructive feedback resulting in increased team productivity.",
+    skills: ["C#","Asp.Net Core Web API",".Net Framework","Microservices","Unit Testing (XUnit)","React JS"],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://static.vecteezy.com/system/resources/previews/020/336/451/original/infosys-logo-infosys-icon-free-free-vector.jpg",
+    role: "System Engineer Trainee",
+    company: "Infosys Ltd",
+    date: "17th February 2022 – 28th April 2022",
+    desc: "Received training in C# Programming Language and MS SQL. Developed Data Access Layer using Entity Framework Core and Web Service Layer using ASP.NET Core Web API for API Development.",
+    skills: ["Java","C#","Asp.Net Core Web API","MySQL","MSSQL"],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://ritgoa.ac.in/wp-content/uploads/2021/06/Anant-Infomedia-Pvt.-Ltd..jpg",
+    role: "Fullstack Externship",
+    company: "Anant Infomedia Pvt Ltd",
+    date: "October 2021 - February 2022",
+    desc: "Contributed to coding, application testing and debugging websites for various projects. Implemented new features from scratch by modifying frontend and backend web pages. Utilized technologies and frameworks such as HTML, CSS, JavaScript, PHP, Angular, CodeIgniter and SQL Server.",
+    skills: ["HTML","CSS","JavaScript","PHP","Angular","CodeIgniter","SQL Server",],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://pccegoa.edu.in/wp-content/uploads/2022/03/cropped-NEW-PCCE-LOGO.png",
+    school: "Padre Conceicao College of Engineering – Goa University",
+    degree: "Bachelor’s Degree in Mechanical Engineering",
+    date: "2017-2021",
+    grade: "70.6%",
+  },
+  {
+    id: 1,
+    img: "https://www.vidyavikasacademy.edu.in/new/wp-content/uploads/2021/06/logopage-235x300.png",
+    school: "Vidya Vikas Academy - Goa",
+    date: "2015-2017",
+    grade: "72.8%",
+    degree: "Higher Secondary (12th)",
+  },
+  {
+    id: 2,
+    img: "https://www.vidyavikasacademy.edu.in/new/wp-content/uploads/2021/06/logopage-235x300.png",
+    school: "Vidya Vikas Academy - Goa",
+    date: "2015",
+    grade: "72.8%",
+    degree: "Senior Secondary (10th)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Trackify",
+    webapp: "https://trackify.duckdns.org",
+  },
+];
